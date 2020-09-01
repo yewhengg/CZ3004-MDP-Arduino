5b0ae4c0292cb6f38b61e6b2a5801673c5e42935
Yew Heng <62707223+yewhengg@users.noreply.github.com>
2020-09-01 15:47:34 +0800

    Update Arduino-Explorer.ino

--- Arduino-Explorer.ino
+++ Arduino-Explorer.ino
@@ -1,5 +1,6 @@
-https://github.com/cheyanniee/Multidisciplinary_Design_Project_CZ3004/blob/master/Arduino/Integrated_code_final/Integrated_code_final.ino
-https://github.com/shiheng7/CZ3004-Arduino/blob/master/MDP_GRP9_Arduino/MDP_GRP9_Arduino.ino
+// https://github.com/nigelawj/cz3004
+// https://github.com/cheyanniee/Multidisciplinary_Design_Project_CZ3004/blob/master/Arduino/Integrated_code_final/Integrated_code_final.ino
+// https://github.com/shiheng7/CZ3004-Arduino/blob/master/MDP_GRP9_Arduino/MDP_GRP9_Arduino.ino
 
 // Motor Library
 // https://github.com/pololu/dual-vnh5019-motor-shield
@@ -22,6 +23,14 @@ https://github.com/shiheng7/CZ3004-Arduino/blob/master/MDP_GRP9_Arduino/MDP_GRP9
 
 double leftEncoderValue = 0;
 double rightEncoderValue = 0;
+double rpm_left;
+double rpm_right;
+unsigned long startTimeLeftEncoder;
+unsigned long endTimeLeftEncoder;
+unsigned long startTimeRightEncoder;
+unsigned long endTimeRightEncoder;
+unsigned long durationLeftEncoder;
+unsigned long durationRightEncoder;
 
 DualVNH5019MotorShield md;
 ZSharpIR SharpIR(IR, sharp_sensor_model);
@@ -32,65 +41,56 @@ void setup()
   Serial.begin(115200);
   md.init();
   pinMode(left_encoder_e1a, INPUT);
-  pinMode(left_encoder_e1b, INPUT);
+  //pinMode(left_encoder_e1b, INPUT);
   pinMode(right_encoder_e2a, INPUT);
-  pinMode(right_encoder_e2b, INPUT);
+  //pinMode(right_encoder_e2b, INPUT);
   PCintPort::attachInterrupt(left_encoder_e1a, leftEncoderInc, RISING);
-  PCintPort::attachInterrupt(left_encoder_e1b, leftEncoderInc, RISING);
+  //PCintPort::attachInterrupt(left_encoder_e1b, leftEncoderInc, RISING);
   PCintPort::attachInterrupt(right_encoder_e2a, rightEncoderInc, RISING);
-  PCintPort::attachInterrupt(right_encoder_e2b, rightEncoderInc, RISING);
+  //PCintPort::attachInterrupt(right_encoder_e2b, rightEncoderInc, RISING);
 }
 
 // ===== loop() =====
 void loop()
 {
-  md.setSpeeds(300, 300);
-  // Serial.println(SharpIR.distance());
-  double duration_left_e1a = pulseIn(left_encoder_e1a, HIGH);
-  double duration_left_e1b = pulseIn(left_encoder_e1b, HIGH);
-  double duration_right_e2a = pulseIn(right_encoder_e2a, HIGH);
-  double duration_right_e2b = pulseIn(right_encoder_e2b, HIGH);
-  Serial.print("Left 1A: ");
-  Serial.println(duration_left_e1a);
-  Serial.print("Left 1B: ");
-  Serial.println(duration_left_e1b);
-  Serial.print("Right 2A: ");
-  Serial.println(duration_right_e2a);
-  Serial.print("Right 2B: ");
-  Serial.println(duration_right_e2b);
-
-  float rpm = leftEncoderValue / 
-  long ST = millis();
-  getRPM(ST);
+  md.setSpeeds(400, 400);
+  // Serial.println(SharpIR.distance())
 }
 
 void leftEncoderInc(void) {
   leftEncoderValue++;
-
+  // Serial.println(leftEncoderValue);
   if (leftEncoderValue == 1) {
-
+    startTimeLeftEncoder = micros();
+  } else if (leftEncoderValue == 101) {
+    endTimeLeftEncoder = micros();
+    durationLeftEncoder = (endTimeLeftEncoder - startTimeLeftEncoder) / 100;
+    rpm_left = 60.00 / (durationLeftEncoder * 562.25 / 1000000);
+    Serial.print("Left RPM: ");
+    Serial.println(rpm_left);
+    leftEncoderValue = 0;
   }
-  
 }
 
 void rightEncoderInc(void) {
   rightEncoderValue++;
-}
-
-void countPulse_MR() {
-  counter_MR++;
-  if(counter_MR == 1) startTime_MR = micros();
-  else if(counter_MR == 11) {
-    endTime_MR = micros();
-    duration_MR = (endTime_MR - startTime_MR) / 10.0;
-    input_MR = calculateRPM(duration_MR);
-    counter_MR = 0;
+  // Serial.print("Right Encoder Value: ");
+  // Serial.println(rightEncoderValue);
+  if (rightEncoderValue == 1) {
+    startTimeRightEncoder = micros();
+    // Serial.print("Start Time Value: ");
+    // Serial.println(startTimeRightEncoder);
+  } else if (rightEncoderValue == 101) {
+    endTimeRightEncoder = micros();
+    // Serial.print("End Time Value: ");
+    // Serial.println(endTimeRightEncoder);
+    durationRightEncoder = (endTimeRightEncoder - startTimeRightEncoder) / 100;
+    // Serial.print("Duration Value: ");
+    // Serial.println(durationRightEncoder);
+    rpm_right = 60.00 / (durationRightEncoder * 562.25 / 1000000);
+    Serial.print("Right RPM: ");
+    Serial.println(rpm_right);
+    rightEncoderValue = 0;
   }
-}
-
-
-double calculateRPM(double pulse) {
-  if (pulse == 0) return 0;
-  else return 60.00 / (pulse *  562.25 / 1000000.00);
 }
   
